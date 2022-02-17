3896c1b4f4d6b7de34949e2290ce952ee4946981
ekzaras <ekz209@nyu.edu>
2022-02-17 14:08:48 -0500

    update scripts

--- js/scripts.js
+++ js/scripts.js
@@ -7,15 +7,21 @@ $.getJSON('/data/gardendata.json', function(gardenData) {
       container: 'mapContainer', // HTML container id
       style: 'mapbox://styles/mapbox/dark-v9', // style URL
       center: cairoCenter, // starting position as [lng, lat]
-      zoom: 11,
+      zoom: 11
     });
+//popups for Gardens
+gardenData.forEach(function(gardenData){
+var popupHTML = `
+  <h3>${gardenData.gardenname}</h3>
+  <p><b>History:</b> ${gardenData.subtext}
+`
+}
+var popup = new mapboxgl.Popup({offset: 40})
+.setHTML(popupHTML);
 
-gardenData.forEach(function(gardenData) {
-var popup = new mapboxgl.Popup({
-    offset: 40,
-  })
-    .setHTML(`<p>${gardenData.gardenname}${gardenData.subtext}</p>`);
-
+// Set all markers to medium green
+var color = '#7EB851'
+    
 new mapboxgl.Marker()
       .setlnglat([gardenData.longitude, gardenData.latitude])
       .setPopup(popup)

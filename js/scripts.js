ec308cf186bbaeb0c0f37e61c259f20c8121cbed
ekzaras <ekz209@nyu.edu>
2022-02-17 01:09:08 -0500

    adding body and data

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,22 @@
 $.getJSON('/data/gardendata.json', function(gardenData) {
-  console.log(gardenData)
+$.getJSON('data/gardendata.json',function(gardenData){
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZWt6YXJhcyIsImEiOiJja3pobm50eDkwaWtvMnZtcHlnOWk5eHB5In0.jfdIdr4Pie4_IpMdk97t8Q'
 
 var cairoCenter = [31.233334, 30.033333]
 var map = new mapboxgl.Map({
     container: 'mapContainer', // HTML container id
-    style: 'mapbox://styles/mapbox/streets-v9', // style URL
+    style: 'mapbox://styles/mapbox/dark-v9', // style URL
     center: cairoCenter, // starting position as [lng, lat]
     zoom: 11,
 });
+gardenData.forEach(function(gardenData) {
 var popup = new mapboxgl.Popup({
     offset: 40,
   })
+    .setHTML(<p>${gardenData.gardenname}${gardenData.subtext}</p>);
 
-    .setHTML('<h3>Cairo</h3>');
-
-//two options for markers
-//cairo = [29.329033,29.388738]
-  var marker = new mapboxgl.Marker()
-      .setlnglat(cairoCenter)
+new mapboxgl.Marker()
+      .setlnglat([gardenData.longitude, gardenData.latitude])
       .setPopup(popup)
       .addTo(map);
-
-  var pointsOfInterest = [
-    {//come up with data format for this
-      lngLat: [31.227425,30.046398],//an array]
-      popupHtml: ['Al Andalus Garden'],
-      subText: 'Built by Zulfugar Pasha in 1929'
-    },
-    {//come up with data format for this
-      lngLat: [31.226781,30.042847],//an array]
-      popupHtml: ['Al Horreya Garden'],
-      subText: 'Originally part of larger green space of Gezira before development'
-    },
-  ]

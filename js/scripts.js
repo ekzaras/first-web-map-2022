1e6ad52d0233b4cf5bc0ec6fe56f631925d269c0
ekzaras <ekz209@nyu.edu>
2022-02-17 16:05:55 -0500

    fix markers and token

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-$.getJSON('/data/gardenrows.json', function(gardenRows) {
-
-    mapboxgl.accessToken = 'pk.eyJ1IjoiZWt6YXJhcyIsImEiOiJja3pxbHJuYzAzOWFzMm9xcmthNHhnOGFkIn0.GQ6837HQ8L2Jym-fwcXT5A'
+$.getJSON('data/gardenrows.json', function(gardenRows) {
+    console.log(gardenRows)
+    mapboxgl.accessToken = 'pk.eyJ1IjoiZWt6YXJhcyIsImEiOiJja3pobm50eDkwaWtvMnZtcHlnOWk5eHB5In0.jfdIdr4Pie4_IpMdk97t8Q'
 
     var cairoCenter = [31.233334, 30.033333]
     var map = new mapboxgl.Map({
@@ -11,18 +11,17 @@ $.getJSON('/data/gardenrows.json', function(gardenRows) {
     });
 //popups for Gardens
 gardenRows.forEach(function(gardenRow){
+  var popup = new mapboxgl.Popup({offset: 40})
+  .setHTML(`popupHTML`);
   var popupHTML = `
   <h3>${gardenRow.gardenname}</h3>
   <p><b>History:</b> ${gardenRow.subtext}
   `
-})
-
-  var popup = new mapboxgl.Popup({offset: 40})
-  .setHTML(`popupHTML`);
-  var color = '#7EB851'
-
   new mapboxgl.Marker()
       .setLngLat([gardenRow.longitude, gardenRow.latitude])
       .setPopup(popup)
       .addTo(map);
+})
+
+
 })
